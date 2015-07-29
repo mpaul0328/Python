9eebdb94f7ef0658510e3399b6159258cf340aa1
Manuel <paul.manuel.pasqual@gmail.com>
2015-07-29 16:30:51 -0700

    List Version: Input is a list file

--- FindExpTime.py
+++ FindExpTime.py
@@ -1,17 +1,19 @@
-'''run FindExpTime.py File1 File2 File3... Output File''' 
+''' run FindExpTime.py InputListFile OutputFile
 
-# DOES NOT OUTPUT TO ANY FILE YET! INPUT EXTRA FILE IN PLACE OF OUTPUT FILE
-
-''' Reads in a bunch of image files and then prints the exposure 
+    Reads in a bunch of image files and then prints the exposure 
 times of each image into another file'''
 
-import sys, numpy as np
+from sys import argv
 from astropy.io import fits 
 
-length = len(sys.argv)
-for arg in range(length-1):
-    if arg != 0:
-        filename = fits.open(sys.argv[arg])
-        time = filename[0].header['EXPTIME']
-        print sys.argv[arg] + " ExpTime = " + str(time)
-
+flist = open(argv[1], 'r')
+outfile = open(argv[2], 'a')
+for file in flist:
+    file = file.replace("\n","")
+    filename = fits.open(file)
+    time = filename[0].header['EXPTIME']
+    outfile.write(file + " ExpTime = " + str(time) + "\n")
+filename.close()
+outfile.close()
+        
+        
