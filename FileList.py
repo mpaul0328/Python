ee1e07d136b2671d381063c75d515634e109d109
Manuel <paul.manuel.pasqual@gmail.com>
2015-07-30 14:48:46 -0700

    Creates a file with a list of .fts files

--- FileList.py
+++ FileList.py
@@ -1,6 +1,9 @@
-''' run data.py OutputFile 
+''' run FileList.py OutputFile 
 
-    Creates a file with a list of .fts files'''
+    Creates a file with a list of .fts files
+
+30-JUL-2015
+Written by Manuel Paul (paulm302@coyote.csusb.edu)'''
 
 from sys import argv
 import glob
@@ -8,6 +11,4 @@ import glob
 data = open(argv[1], "a")
 for i in glob.glob("*.fts"):
     data.write(i + "\n")
-
 data.close()
-
